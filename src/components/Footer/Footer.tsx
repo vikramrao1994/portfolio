78c7c55f8ad926c33f0b0d2a03963bbfebb15d59
Vikram Rao <rao@publicplan.de>
2025-10-25 10:55:39 +0200

    feat: add footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,24 @@
+'use client';
+import { Grid, Preline, Subline } from "@publicplan/kern-react-kit";
+import { SITE } from "@/lib/content";
+import pkg from "../../../package.json";
+
 const Footer = () => {
-  return <p>© 2023 Vikram Rao. All rights reserved.</p>;
+  return (
+    <Grid
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+      }}
+    >
+      <Subline text={`© ${new Date().getFullYear()} ${SITE.heading.name} — Frontend Engineer`} variant="small"/>
+      <Preline text={`Built with Next.js v${pkg.dependencies.next} & TypeScript · Deployed on Github Pages`} variant="small"/>
+    </Grid>
+  );
 };
 
 export default Footer;
+
